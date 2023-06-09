ebd95a04b87994b2b00db48320c1b728b34b2c4e
Anonymous <anonymous@anywhere.local>
2023-06-10 00:34:45 +0800

    update

--- Formula/bbdown.rb
+++ Formula/bbdown.rb
@@ -2,10 +2,10 @@ class Bbdown < Formula
   desc "Bilibili Downloader 命令行式哔哩哔哩下载器"
   homepage "https://github.com/nilaoda/BBDown/"
   url "https://github.com/nilaoda/BBDown/archive/refs/tags/#{version}.tar.gz"
-  version "1.5.7"
+  version "1.5.8"
   license "MIT"
 
-  date = "20230530"
+  date = "20230608"
 
   if OS.mac?
     if Hardware::CPU.arm?
