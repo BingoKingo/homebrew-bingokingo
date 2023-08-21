194f82e02484a9b6fecbb38629c20e7232d0cc9b
Anonymous <anonymous@anywhere.local>
2023-08-21 22:33:37 +0800

    upgrade alist

--- Formula/alist.rb
+++ Formula/alist.rb
@@ -3,17 +3,20 @@ class Alist < Formula
   homepage "https://alist.nn.ci/"
   url "https://github.com/alist-org/alist/archive/refs/tags/v#{version}.tar.gz",
       verified: "github.com/alist-org/alist/"
-  version "3.24.0"
+  version "3.26.0"
   license "AGPL-3.0-only"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/alist-org/alist/releases/download/v#{version}/alist-darwin-arm64.tar.gz"
+      sha256 "8fc01c603e1f48a72e0d47eb9f283ceaecd97672890b776a3c183d9e8e9ec1ac"
     elsif Hardware::CPU.intel?
       url "https://github.com/alist-org/alist/releases/download/v#{version}/alist-darwin-amd64.tar.gz"
+      sha256 "d4d64db13fc1f52404c8c4e347bb67d9ec1cc45b96cd6b2829a30b8227cce6aa"
     end
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/alist-org/alist/releases/download/v#{version}/alist-linux-amd64.tar.gz"
+    sha256 "555a8482aeedb15a0e28a7a6e19868d417816d3f23b37b68cab597b7bfc10c9a"
   end
 
   livecheck do
@@ -33,19 +36,18 @@ class Alist < Formula
   end
 
   service do
-    run [opt_bin/"alist", "server", "--data", opt_prefix/"data"]
+    run [opt_bin/"alist", "server"]
     working_dir opt_prefix
     keep_alive true
   end
 
   def caveats
     <<~EOS
-      To reveal alist admin user's info again, run the following command:
-      cd #{opt_prefix} && alist admin
+      To reveal alist admin user's info in default `config.json` again, run the following command:
+        cd #{opt_prefix} && alist admin
       Or reveal password via `sqlite3` command:
-      sqlite3 #{etc}/alist/data.db "select password from x_users where username = 'admin'"
-      NOTICE: `--data` parameter may not work. If you need to run elsewhere with same configuration, please navigate to `#{opt_prefix}` first.
-      EOS
+        sqlite3 #{etc}/alist/data.db "select password from x_users where username = 'admin'"
+    EOS
   end
 
   test do
