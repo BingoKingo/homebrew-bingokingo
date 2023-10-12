b14fe31ec537367e79db13f1be5d8ff32cd3bf40
Anonymous <anonymous@anywhere.local>
2023-10-12 16:47:47 +0800

    add reality converter

---
+++ Casks/reality-converter.rb
@@ -0,0 +1,24 @@
+cask "reality-converter" do
+  version "1.0,52"
+  sha256 :no_check
+
+  url "https://developer.apple.com/augmented-reality/tools/files/Reality_Converter_beta_5.dmg"
+  name "Reality Converter"
+  desc "Convert, view, and customize USDZ 3D objects"
+  homepage "https://developer.apple.com/download/applications/"
+
+  depends_on macos: ">= :monterey"
+  container nested: "RealityConverter.pkg"
+
+  pkg "RealityConverter.pkg"
+
+  uninstall pkgutil: [
+    "com.apple.pkg.RealityConverter",
+  ]
+
+  zap trash: [
+    "~/Library/Application Scripts/com.apple.RealityConverter",
+    "~/Library/Caches/com.apple.helpd/Generated/com.apple.RealityConverter.help*1.0",
+    "~/Library/Containers/com.apple.RealityConverter",
+  ]
+end
