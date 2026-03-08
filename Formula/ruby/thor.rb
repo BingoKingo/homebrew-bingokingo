61290d8cae3ca45391a3782644aef124410a0fa4
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2026-03-08 06:38:47 +0000

    thor: add 1.5.0 bottle.

--- Formula/ruby/thor.rb
+++ Formula/ruby/thor.rb
@@ -6,6 +6,11 @@ class Thor < Formula
   license "MIT"
   head "https://github.com/rails/thor.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bingokingo/homebrew"
+    sha256 cellar: :any_skip_relocation, all: "b9ac0dfa72f29271e54ce3beb3a1af14f212f994f8cc21c2e06503500595f1e5"
+  end
+
   depends_on "ruby"
   # uses_from_macos "ruby"
 
