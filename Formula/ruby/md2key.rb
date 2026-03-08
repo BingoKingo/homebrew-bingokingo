beb6cacad2fd8804e1e7cf2240a31e7abafacfee
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2026-03-08 06:38:47 +0000

    md2key: add 0.10.2 bottle.

--- Formula/ruby/md2key.rb
+++ Formula/ruby/md2key.rb
@@ -6,6 +6,15 @@ class Md2key < Formula
   license "MIT"
   head "https://github.com/k0kubun/md2key.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bingokingo/homebrew"
+    sha256 cellar: :any,                 arm64_tahoe:   "7085fc22512d7be697b063f005e1832d0817c02efedc07089dfa53ac1f762cd8"
+    sha256 cellar: :any,                 arm64_sequoia: "fd6b5c69ab8fe1abcff29f7e6080042a7d555ba89061c698c84b7b805c7068ca"
+    sha256 cellar: :any,                 arm64_sonoma:  "61410c85566f8911447d7083ae5ec51046cdc2d2b9a807cf6228e33a6969fe6f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5d87460b80ba8be25b9546bc384b4d9b8eea4d8a486857ff7ac181481341abd2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1524ded3ff91d10e7cdb98d8a13c94994717249ef90f4df29bbbf69ce5dbb999"
+  end
+
   depends_on "ruby"
   # uses_from_macos "ruby"
 
