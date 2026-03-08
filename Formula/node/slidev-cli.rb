5da6dac34c235051532c4082f9276a70e8ee6b4c
BingoKingo <24961267+BingoKingo@users.noreply.github.com>
2026-03-08 03:47:15 +0000

    slidev-cli: add 52.11.5 bottle.

--- Formula/node/slidev-cli.rb
+++ Formula/node/slidev-cli.rb
@@ -6,6 +6,15 @@ class SlidevCli < Formula
   license "MIT"
   head "https://github.com/slidevjs/slidev.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/bingokingo/homebrew"
+    sha256 cellar: :any,                 arm64_tahoe:   "a90be02de20e275dee4e38a614790ce980653cb8f0872980f33e54b22e77a196"
+    sha256 cellar: :any,                 arm64_sequoia: "bcdfb858ed3dc9898973dde61b180cdb67a9a5581420487ea0c87c8828c3e9c2"
+    sha256 cellar: :any,                 arm64_sonoma:  "bcdfb858ed3dc9898973dde61b180cdb67a9a5581420487ea0c87c8828c3e9c2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "453b79f4d3921718c974270c61e908b25907f80bb7384b52a56c3f9f98d6a253"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f833c28e104761bc0ff07e800b45f0777aa6eed26f277a064a14b4c262850783"
+  end
+
   depends_on "node"
 
   def install
